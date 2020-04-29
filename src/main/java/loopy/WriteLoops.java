5da4eb1c5788f961809843b575213011d542107d
Edmund Kotei <edmundkotei@gmail.com>
2020-04-29 01:20:20 -0400

    Initial commit

---
+++ src/main/java/loopy/WriteLoops.java
@@ -0,0 +1,113 @@
+package loopy;
+import java.awt.SystemTray;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+
+public class WriteLoops {
+    private static final int _3 = 3;
+
+
+public int oneToFive() {
+    int w = 0;
+    for (w = 0; w <= 3; w++) {
+
+
+
+    }
+    return w = w + 1;
+}
+
+
+    public int oneToTen() {
+        int w = 0;
+        for(w = 0; w <= 8; w++){
+
+        }
+        return w = w + 1;
+    }
+
+    public int startAtTwentyOne() {
+        int w = 0;
+        for(w = 0; w <= 9; w++){
+
+        }
+        return w = w + 1;
+    }
+
+    public int countDown() {
+        int w = 0;
+        for(w = 0; w <= 98; w++){
+
+        }
+        return w = w + 1;
+    }
+
+    public int byTwoTo32() {
+        return 0;
+    }
+
+    public int countDownFrom5000() {
+    int w = 0;
+    for(w = 0; w <= 453; w++){
+
+    }
+        return w = w + 1;
+    }
+
+    public int nestedFors() {
+    int w = 0;
+    int i = 0;
+    for(w = 0; w <= 98; w++){
+for(i = 0; i < 5; i++){
+
+}
+    }
+        return w = w + 1;
+    }
+
+    public int helloZipCode() {
+    int w = 0;
+    for(w = 5; w <= 45; w++){
+    if(w > 51){
+        System.out.println("Hello Zipcode");
+    }
+    }
+        return w = w + 1;
+    }
+
+    public int driveHome() {
+        int w = 0;
+       while(w < 5){
+        w++;
+       }
+            return w = w + 1;
+    }
+
+    public int checkGameScore() {
+    int w = 0;
+    for(w = 0; w <= 1; w++){
+
+    }
+        return w = w + 1;
+    }
+
+    public boolean checkGameScoreDoWhile() {
+        int w = 0;
+        int highestScore = 236;
+        int currentScore = gameNextScore();
+        int runningScore = 0;
+
+
+        w = w + 1;
+        return w >= 3;
+    }
+
+    private int gameNextScore() {
+        return 0;
+    }
+
+    public int checkServerStatus() {
+        return 0;
+    }
+}
+
